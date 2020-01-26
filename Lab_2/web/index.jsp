54e36047392bea6023b3697c527c08e0d4fc7ea5
avvessalom <avvessalom@gmail.com>
2020-01-26 17:09:39 +0300

    q

--- Lab_2/web/index.jsp
+++ Lab_2/web/index.jsp
@@ -1,16 +1,86 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: kain
-  Date: 25.01.2020
-  Time: 14:10
-  To change this template use File | Settings | File Templates.
---%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<html>
-  <head>
-    <title>$Title$</title>
-  </head>
-  <body>
-  $END$
-  </body>
+<html lang=ru xmlns:https="http://www.w3.org/1999/xhtml">
+<head>
+  <meta charset="UTF-8">
+  <title>Lab_2</title>
+  <link rel="stylesheet" href="main.css">
+  <link href="https://fonts.googleapis.com/css?family=Oswald&display=swap" rel="stylesheet">
+  <script src="scripts.js"></script>
+</head>
+<body onload="drawCanwas('canvas',1)">
+<header class="header">
+  <div class="container">
+    <div class="name">
+      <span>Lazurin Eugene</span>
+    </div>
+    <div class="option">
+      <span> Вариант 1421</span>
+    </div>
+  </div>
+</header>
+
+<footer class="footer">
+  <div class="container">
+    <div class="footer-inner">
+      <span>&copy; Лазурин Евгений</span>
+    </div>
+  </div>
+</footer>
+
+<table class="content" cellspacing="50 px">
+  <tr>
+    <td >
+      <canvas id="canvas" onclick="clicCanvas()" style="background-color:#ffffff; border-radius: 20px;" width="300" height="300"></canvas>
+
+    </td>
+    <td>
+      <form class="data-send-form" id="form" action="checking" method="get" onsubmit="return validate(this);">
+        <table class="form-table">
+          <tr>
+            <td><div class="Xvalue">
+              <label class="X">X Value:</label>
+              <label for="X"></label>
+              <input type="text" id="X" name="X" required>
+            </div> </td>
+            <td><div class="Rvalue">
+              <label class="R">R Value:</label>
+              <label for="R"></label>
+              <select id="R" name="R" required>
+                <option onclick="drawCanwas('canvas',1)" value="1">1</option>
+                <option onclick="drawCanwas('canvas',1.5)" value="1.5">1.5</option>
+                <option onclick="drawCanwas('canvas',2)" value="2">2</option>
+                <option onclick="drawCanwas('canvas',2.5)" value="2.5">2.5</option>
+                <option onclick="drawCanwas('canvas',3)" value="3">3</option>
+              </select>
+            </div></td>
+
+          </tr>
+          <tr>
+            <td><div>
+              <label class="Y">Y value:</label>
+
+              <button name="Y" value="-5">-5</button>
+              <button name="Y" value="-4">-4</button>
+              <button name="Y" value="-3">-3</button>
+              <div>
+                <button name="Y" value="-2">-2</button>
+                <button name="Y" value="-1">-1</button>
+                <button name="Y" value="0">0</button>
+              </div>
+              <div>
+                <button name="Y" value="1">1</button>
+                <button name="Y" value="2">2</button>
+                <button name="Y" value="3">3</button>
+              </div>
+            </div></td>
+            <td>
+              <input class="submit" type="submit" name="submit" value="Проверить">
+            </td>
+          </tr>
+        </table>
+      </form>
+    </td>
+  </tr>
+</table>
+</body>
 </html>
---
+++ Lab_2/web/scripts.js
@@ -0,0 +1,150 @@
+var timeout;
+
+function validate(_form){
+
+    var fail = false;
+    var X = _form.X.value;
+    var Y = _form.Y.value;
+    var R = _form.R.value;
+
+    if (X <= -3 || X >= 5 || isNaN(X) || X == "" || X.length > 10){
+        fail = "X value is incorrect ! \n";
+    }
+
+    if (fail){
+        alert(fail);
+        return false;
+    }
+    else{
+        createCanvas('canvas', X, Y, R);
+        return true;
+    }
+
+}
+
+//--------------------------------------------------------------------
+
+function drawCanwas(id, r){
+    var canvas = document.getElementById(id),
+        context = canvas.getContext("2d");
+    //очистка
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    //прямоугольник
+    context.beginPath();
+    context.rect(150, 20, 130, 130);
+    context.closePath();
+    context.strokeStyle = "blue";
+    context.fillStyle = "blue";
+    context.fill();
+    context.stroke();
+
+    // сектор
+    context.beginPath();
+    context.moveTo(150, 150);
+    context.arc(150, 150, 65, 3*Math.PI/2, Math.PI, true );
+    context.closePath();
+    context.strokeStyle = "blue";
+    context.fillStyle = "blue";
+    context.fill();
+    context.stroke();
+
+    //треугольник
+    context.beginPath();
+    context.moveTo(215, 150);
+    context.lineTo(150, 280);
+    context.lineTo(150, 150);
+    context.lineTo(85, 150);
+    context.closePath();
+    context.strokeStyle = "blue";
+    context.fillStyle = "blue";
+    context.fill();
+    context.stroke();
+
+    //отрисовка осей
+    context.beginPath();
+    context.font = "10px Verdana";
+    context.moveTo(150, 0); context.lineTo(150, 300);
+    context.moveTo(150, 0); context.lineTo(145, 15);
+    context.moveTo(150, 0); context.lineTo(155, 15);
+    context.fillText("Y", 160, 10);
+    context.moveTo(0, 150); context.lineTo(300, 150);
+    context.moveTo(300, 150); context.lineTo(285, 145);
+    context.moveTo(300, 150); context.lineTo(285, 155);
+    context.fillText("X", 290, 135);
+
+    // деления X
+    context.moveTo(145, 20); context.lineTo(155, 20); context.fillText(r, 160, 20);
+    context.moveTo(145, 85); context.lineTo(155, 85); context.fillText((r / 2), 160, 78);
+    context.moveTo(145, 215); context.lineTo(155, 215); context.fillText(-(r / 2), 160, 215);
+    context.moveTo(145, 280); context.lineTo(155, 280); context.fillText(-r, 160, 280);
+    // деления Y
+    context.moveTo(20, 145); context.lineTo(20, 155); context.fillText(-r, 20, 170);
+    context.moveTo(85, 145); context.lineTo(85, 155); context.fillText(-(r / 2), 70, 170);
+    context.moveTo(215, 145); context.lineTo(215, 155); context.fillText((r / 2), 215, 170);
+    context.moveTo(280, 145); context.lineTo(280, 155); context.fillText(r, 280, 170);
+
+    context.closePath();
+    context.strokeStyle = "black";
+    context.fillStyle = "black";
+    context.stroke();
+}
+
+function createCanvas(id, x, y, r){
+
+    drawCanwas(id, r);
+    context.beginPath();
+    context.rect(Math.round(150 + ((x / r) * 130))-2, Math.round(150 - ((y / r) * 130))-2, 4, 4);
+    context.closePath();
+    context.strokeStyle = "red";
+    context.fillStyle = "red";
+    context.fill();
+    context.stroke();
+
+}
+
+function clicCanvas(canvId, R) {
+    var elem = document.getElementById(canvId);
+    var br = elem.getBoundingClientRect();
+    var left = br.left;
+    var top = br.top;
+    var event = window.event;
+    var x = event.clientX-left;
+    var y = event.clientY-top;
+    var boolArea = isArea(x, y, R);
+    drawPoint(canvId, x, y, boolArea);
+}
+
+function isArea(x, y, R) {
+    x = (x - 150) / 130;
+    y = (150 - y) / 130;
+    if(x<=0 && y<=0 && x*x+y*y<=R*R){
+        return 'true';
+    }
+    if(x<=0 && y>=0 && y<=(2.0*x+1.0*R)){
+        return 'true';
+    }
+    if(x>=0 && y<=0 && x<=R && y>=-R){
+        return 'true';
+    }
+    return 'false';
+}
+
+function drawPoint(id, x, y, isArea){
+    var canvas = document.getElementById(id),
+        context = canvas.getContext("2d");
+
+    context.beginPath();
+    context.rect(x - 2, y - 2, 4, 4);
+    context.closePath();
+    if(isArea === 'true'){
+        context.strokeStyle = "green";
+        context.fillStyle = "green";
+    } else {
+        context.strokeStyle = "red";
+        context.fillStyle = "red";
+    }
+    context.fill();
+    context.stroke();
+
+}
